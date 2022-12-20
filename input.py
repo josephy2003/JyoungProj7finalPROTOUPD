90a76048903382091b7a71fa82236d00e40d1213
_ixmnxbdy111 <65754413+josephy2003@users.noreply.github.com>
2022-12-20 01:38:29 -0500

    Initial commit

---
+++ input.py
@@ -0,0 +1,65 @@
+import arcade
+import sys
+
+print (sys.getrecursionlimit())
+
+
+
+v = VARIABLES
+n = NUMBERS
+i = INTEGER
+
+class Formula:
+    def __init__(self, v, n, i):
+    self.paramter
+
+def
+    vars(P) == 100000
+    vars(T) == 40
+    vars(R) == .07
+    vars(N) == 1
+    vars(A) == P * (1 + r / n) ** (n * t)
+    vars(B) == round(A, 2)
+    vars(C) == '{:,}'.format(B)
+def loop_function(n):
+    result = 1
+    for number in range(1,n):
+        result = result * number
+    return n*result
+
+def fact(n)
+        if n<=1
+                return 1
+        return n * fact(n-1)
+
+A = P(1 + r/n)nt
+
+def factorial(number):
+    product = 1
+    for i in range(number):
+        product = product * (i+i)
+
+
+number = input("Enter '-' integer to take the factorial of:")
+
+
+if __name__ = __init__
+
+
+#function getFactorial (5)
+#   factorial = 1
+#   for x 1 to 5
+#       factorial = factorial * x
+
+#Recursion-Func
+
+#5! 5*4*3*2*1
+
+getFactorial = factorial("number")
+
+class gFactorial(self):
+    def __init__(self, factorial,number):
+            self.factorial(1) == get.Factorial(number)
+            self.factorial(2) == getFactorial(number)
+            self.factorial(3)== getFactorial(number)
+            self.factorial(4) == getFactorial(number)
